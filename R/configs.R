119716938352577e63e41dd5173e3296aa4ab38d
Ryan Price <ryapric@gmail.com>
2017-12-20 18:33:47 -0600

    Got functions working to pull URLs, and download requested data. Robust to character case.

--- R/configs.R
+++ R/configs.R
@@ -1,63 +1,51 @@
-# How the static web addresses begin...
-static_web_address_head <- "http://files.zillowstatic.com/research/public/"
+# How the static web addresses begin
+static_url_head <- "http://files.zillowstatic.com/research/public/"
 
-# ...and how they end, for each variable's static download link
-static_web_address_tail <- list(
-  "home" = list("_Zhvi_Summary_AllHomes.csv",
-                "_Zhvi_AllHomes.csv",
-                "_Zhvi_BottomTier.csv",
-                "_Zhvi_TopTier.csv",
-                "_Zhvi_Condominum.csv",
-                "_Zhvi_SingleFamilyResidence.csv",
-                "_Zhvi_1bedroom.csv",
-                "_Zhvi_2bedroom.csv",
-                "_Zhvi_3bedroom.csv",
-                "_Zhvi_4bedroom.csv",
-                "_Zhvi_5BedroomOrMore.csv",
-                "_MedianValuePerSqft_AllHomes.csv"
-  ),
-  "rental" = list("_Zri_AllHomesPlusMultifamily_Summary.csv",
-                  "_Zri_AllHomesPlusMultifamily.csv",
-                  "_Zri_AllHomes.csv",
-                  "_Zri_SingleFamilyResidenceRental.csv",
-                  "_Zri_MultiFamilyResidenceRental.csv",
-                  "_ZriPerSqft_AllHomes.csv",
-                  "_MedianRentalPrice_AllHomes.csv",
-                  "_MedianRentalPrice_Mfr5Plus.csv",
-                  "_MedianRentalPrice_CondoCoop.csv",
-                  "_MedianRentalPrice_DuplexTriplex.csv",
-                  "_MedianRentalPrice_Sfr.csv",
-                  "_MedianRentalPrice_Studio.csv",
-                  "_MedianRentalPrice_1Bedroom.csv",
-                  "_MedianRentalPrice_2Bedroom.csv",
-                  "_MedianRentalPrice_3Bedroom.csv",
-                  "_MedianRentalPrice_4Bedroom.csv",
-                  "_MedianRentalPrice_5BedroomOrMore.csv",
-                  "_MedianRentalPricePerSqft_AllHomes.csv",
-                  "_MedianRentalPricePerSqft_Mfr5Plus.csv",
-                  "_MedianRentalPricePerSqft_CondoCoop.csv",
-                  "_MedianRentalPricePerSqft_DuplexTriplex.csv",
-                  "_MedianRentalPricePerSqft_Sfr.csv",
-                  "_MedianRentalPricePerSqft_Studio.csv",
-                  "_MedianRentalPricePerSqft_1Bedroom.csv",
-                  "_MedianRentalPricePerSqft_2Bedroom.csv",
-                  "_MedianRentalPricePerSqft_3Bedroom.csv",
-                  "_MedianRentalPricePerSqft_4Bedroom.csv",
-                  "_MedianRentalPricePerSqft_5BedroomOrMore.csv"))
+# List of short-form names to be used as the zillow_var arguments
+# The values of this list just need to end in ".csv" to become the static URL tails
+zillow_var_dict <- list(
+  "zhvi_summary" = "Zhvi_Summary_AllHomes.csv",
+  "zhvi_allHomes" = "Zhvi_AllHomes.csv",
+  "zhvi_bottomtier" = "Zhvi_BottomTier.csv",
+  "zhvi_toptier" = "Zhvi_TopTier.csv",
+  "zhvi_condo" = "Zhvi_Condominum.csv",
+  "zhvi_sfr" = "Zhvi_SingleFamilyResidence.csv",
+  "zhvi_1Bed" = "Zhvi_1bedroom.csv",
+  "zhvi_2Bed" = "Zhvi_2bedroom.csv",
+  "zhvi_3Bed" = "Zhvi_3bedroom.csv",
+  "zhvi_4Bed" = "Zhvi_4bedroom.csv",
+  "zhvi_5BedPlus" = "Zhvi_5BedroomOrMore.csv",
+  "medValue_psf" = "MedianValuePerSqft_AllHomes.csv",
+  "zri_summary" = "Zri_AllHomesPlusMultifamily_Summary.csv",
+  "zri_allHomesPlus" = "Zri_AllHomesPlusMultifamily.csv",
+  "zri_allHomes" = "Zri_AllHomes.csv",
+  "zri_sfr" = "Zri_SingleFamilyResidenceRental.csv",
+  "zri_mfr" = "Zri_MultiFamilyResidenceRental.csv",
+  "zri_allHomes_psf" = "ZriPerSqft_AllHomes.csv",
+  "medRent_allHomes" = "MedianRentalPrice_AllHomes.csv",
+  "medRent_mfr" = "MedianRentalPrice_Mfr5Plus.csv",
+  "medRent_condo" = "MedianRentalPrice_CondoCoop.csv",
+  "medRent_dtx" = "MedianRentalPrice_DuplexTriplex.csv",
+  "medRent_sfr" = "MedianRentalPrice_Sfr.csv",
+  "medRent_studio" = "MedianRentalPrice_Studio.csv",
+  "medRent_1Bed" = "MedianRentalPrice_1Bedroom.csv",
+  "medRent_2Bed" = "MedianRentalPrice_2Bedroom.csv",
+  "medRent_3Bed" = "MedianRentalPrice_3Bedroom.csv",
+  "medRent_4Bed" = "MedianRentalPrice_4Bedroom.csv",
+  "medRent_5BedPlus" = "MedianRentalPrice_5BedroomOrMore.csv",
+  "medRent_allHomes_psf" = "MedianRentalPricePerSqft_AllHomes.csv",
+  "medRent_mfr_psf" = "MedianRentalPricePerSqft_Mfr5Plus.csv",
+  "medRent_condo_psf" = "MedianRentalPricePerSqft_CondoCoop.csv",
+  "medRent_dtx_psf" = "MedianRentalPricePerSqft_DuplexTriplex.csv",
+  "medRent_sfr_psf" = "MedianRentalPricePerSqft_Sfr.csv",
+  "medRent_studio_psf" = "MedianRentalPricePerSqft_Studio.csv",
+  "medRent_1Bed_psf" = "MedianRentalPricePerSqft_1Bedroom.csv",
+  "medRent_2Bed_psf" = "MedianRentalPricePerSqft_2Bedroom.csv",
+  "medRent_3Bed_psf" = "MedianRentalPricePerSqft_3Bedroom.csv",
+  "medRent_4Bed_psf" = "MedianRentalPricePerSqft_4Bedroom.csv",
+  "medRent_5Bed_psf" = "MedianRentalPricePerSqft_5BedroomOrMore.csv")
 
-# This will be the reference table by which users can review what the argument
-# options are
-zillow_var_ids <- gsub("[[:punct:]]|csv", "", unlist(static_web_address_tail))
-names(zillow_var_ids) <- gsub("home", "home_", names(zillow_var_ids))
-names(zillow_var_ids) <- gsub("rental", "rental_", names(zillow_var_ids))
-zillow_var_ids <- data.frame(variable_name = names(zillow_var_ids),
-                             variable_desc_key = as.character(zillow_var_ids),
-                             row.names = NULL,
-                             stringsAsFactors = FALSE)
-
-
-
-# Geographic levels by which Zillow reports their research data
+# # Zillow's geographical reporting levels
 zillow_data_reporting_level <- list("State",
                                      "Metro",
                                      "County",
@@ -67,21 +55,8 @@ zillow_data_reporting_level <- list("State",
 
 
 
-# Populate full web addresses, based on the above
-static_web_address_full <-
-  lapply(zillow_data_reporting_level,
-         function(x) paste0(static_web_address_head,
-                            x,
-                            "/",
-                            x,
-                            unlist(static_web_address_tail)))
+# # Fuck it, just scrape it
+# page <-
+#   html_session("https://www.zillow.com/research/data/") %>%
+#   html_table()
 
-names(static_web_address_full) <- tolower(unlist(zillow_data_reporting_level))
-
-# static_web_addresses_full <- as.data.frame(static_web_addresses_full)
-
-
-
-# Something to look up full web address via short-form variable name,
-# e.g. "home_1_state" would look up the first Home variable's state-level address
-web_address_config <- list()
--- R/extract.R
+++ R/extract.R
@@ -1,16 +1,68 @@
+#' Determine static web URL address for Zillow data
+#' 
+#' This function is designed to fetch the full URL of Zillow's public research
+#' data via the site's static-address \code{.csv} links. It is specificaly
+#' designed as a utility in actually fetching the data.
+#'
+#' @param zillow_var The variable name to read in. The full list of
+#' these short-form variable names can be found in ...
+#' 
+#' @param geo_level The geographic level of the data to retrieve, passed as a
+#' character string. Available options are: \code{state}, \code{metro},
+#' \code{county}, \code{city}, \code{zip}, and \code{neighborhood}.
+#' 
+#' @return A character string giving the requested URL
+#' 
+#' @export
+#'
+#' @examples
+zillow_get_url <- function(zillow_var, geo_level) {
+  
+  # Throw error if Zillow variable name isn't in list of options
+  if (!(tolower(zillow_var) %in% tolower(names(zillow_var_dict)))) {
+    stop(paste0("Invalid Zillow variable name. Please choose from..."))
+  }
+  
+  # Throw error if geographic level isn't in list of options
+  if (!(tolower(geo_level) %in% tolower(zillow_data_reporting_level))) {
+    stop(paste0("Invalid geographic level. Please choose from this list: ",
+                unlist(tolower(zillow_data_reporting_level)),
+                collapse = ", "))
+  }
+  
+  zillow_var_correct_case <- tolower(zillow_var)
+  
+  geo_level_correct_case <-
+    paste0(toupper(substr(geo_level, 1, 1)),
+           tolower(substr(geo_level, 2, nchar(geo_level))))
+  
+  paste0(static_url_head,
+         geo_level_correct_case, "/", geo_level_correct_case, "_",
+         zillow_var_dict[[zillow_var_correct_case]],
+         ".csv")
+  
+}
+
+
+
 #' Read static Zillow data from the web
 #' 
-#' @description This function is designed to read in Zillow's public research
+#' This function is designed to read in Zillow's public research
 #' data via the site's static-address \code{.csv} links.
 #'
 #' @param zillow_var The variable name to read in. The full list of
-#' these short-form variable names can be found in the \code{zillow_var_ids}
-#' data available in this package.
+#' these short-form variable names can be found in ...
+#' 
+#' @param geo_level The geographic level of the data to retrieve, passed as a
+#' character string. Available options are: \code{state}, \code{metro},
+#' \code{county}, \code{city}, \code{zip}, and \code{neighborhood}.
+#' 
 #' @return A table of class \code{tbl_df}
+#' 
 #' @export
 #'
 #' @examples
 zillow_read_from_web <- function(zillow_var, geo_level) {
-  # readr::read_csv(zillow_var_ids[[geo_level]])
-  print(static_web_address_full[[geo_level]])
+  zillow_url <- zillow_get_url(zillow_var, geo_level)
+  readr::read_csv(zillow_url)
 }
